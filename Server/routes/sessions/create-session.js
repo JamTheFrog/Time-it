a65c50e720044dfc5df123878341804bce93515c
Faris Srnja <faris.srnja2004@gmail.com>
2023-09-30 07:24:08 +0200

    more bug fixes

--- Server/routes/sessions/create-session.js
+++ Server/routes/sessions/create-session.js
@@ -9,7 +9,7 @@ const baseEndpoint = "/api/session";
 router.post(`${baseEndpoint}`, checkAuth, async (req, res) => {
   const { title, timeBlocks, public } = req.body;
   console.log(req.currentUser);
-  if (req.currentUser.type !== "free")
+  if (req.currentUser.id !== session.owner)
     return res.status(403).send("Not Authorized");
   const sessionDoc = await Session.create({
     title,
